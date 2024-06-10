63275d93925f9b27d316ca1cd82ab2a046b66c1c
Aryan Rathore <101472946+aryanrathore63@users.noreply.github.com>
2024-06-10 23:22:13 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Database
+mongoose
+  .connect('mongodb+srv://daria:daria@twittercluster.7shaxg6.mongodb.net/tweeter', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+
+// Server
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
